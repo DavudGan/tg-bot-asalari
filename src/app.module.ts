133c1241bedb40229890a95c2dcafb789df4e3ba
Davud <davud@MacBook-Pro-Davud.local>
2024-11-11 20:20:36 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LevelModule } from './level/level.module';
 
 @Module({
-  imports: [],
+  imports: [LevelModule],
   controllers: [AppController],
   providers: [AppService],
 })
